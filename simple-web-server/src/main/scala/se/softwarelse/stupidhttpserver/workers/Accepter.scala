a8905d2ff3363444f0fa80b16e6964f3f1c5d905
Johan Kjölhede <gigurra@gmail.com>
2021-09-23 23:45:20 +0200

    WIP

--- simple-web-server/src/main/scala/se/softwarelse/stupidhttpserver/workers/Accepter.scala
+++ simple-web-server/src/main/scala/se/softwarelse/stupidhttpserver/workers/Accepter.scala
@@ -4,6 +4,7 @@ import com.invidi.simplewebserver.context.WebServerContext
 
 import java.net.ServerSocket
 import java.util.logging.Logger
+import scala.concurrent.ExecutionContext
 import scala.util.control.NonFatal
 
 class Accepter(serverSocket: ServerSocket,
@@ -15,7 +16,7 @@ class Accepter(serverSocket: ServerSocket,
     while (!serverSocket.isClosed) {
       try {
         val clientSocket = serverSocket.accept()
-        new Thread(new Client(clientSocket, webServerContext)).start() // Better to use a limited thread pool here ;)
+        ExecutionContext.global.execute(new Client(clientSocket, webServerContext)) // Better to use a limited thread pool here ;)
       } catch {
         case NonFatal(e) =>
           log.severe(s"Shit went wront with socket.accept: $e") // stupid java logger cant show me stack traces
