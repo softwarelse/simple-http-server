702e151c12f24d939ebabe2fb39a66851629c3da
Johan Kjölhede <gigurra@gmail.com>
2021-09-24 00:05:01 +0200

    WIP

--- simple-web-server/src/main/scala/se/softwarelse/stupidhttpserver/model/KV.scala
+++ simple-web-server/src/main/scala/se/softwarelse/stupidhttpserver/model/KV.scala
@@ -1,13 +1,7 @@
 package se.softwarelse.stupidhttpserver.model
 
-import se.softwarelse.stupidhttpserver.escapeJson
-
 case class KV(key: String, value: String) {
   def isEmpty: Boolean = key.isEmpty
 
   def nonEmpty: Boolean = !isEmpty
-
-  def toJson: String = {
-    s"""{ "${escapeJson(key)}": "${escapeJson(value)}" }"""
-  }
 }
